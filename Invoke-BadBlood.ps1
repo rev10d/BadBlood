bc88ae5ad308ca158baf84efeb96579fb0c830a7
rev10d <31927659+rev10d@users.noreply.github.com>
2021-01-06 22:27:55 -0700

    Update Invoke-BadBlood.ps1
    All gas no brakes. trashed the cautions.

--- Invoke-BadBlood.ps1
+++ Invoke-BadBlood.ps1
@@ -25,27 +25,7 @@ $basescriptPath = Get-ScriptDirectory
 $totalscripts = 7
 
 $i = 0
-cls
-write-host "Welcome to BadBlood"
-Write-Host  'Press any key to continue...';
-write-host "`n"
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
-write-host "The first tool that absolutely mucks up your TEST domain"
-write-host "This tool is never meant for production and can totally screw up your domain"
-Write-Host  'Press any key to continue...';
-write-host "`n"
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
-Write-Host  'Press any key to continue...';
-write-host "You are responsible for how you use this tool. It is intended for personal use only"
-write-host "This is not intended for commercial use"
-Write-Host  'Press any key to continue...';
-write-host "`n"
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
-write-host "`n"
-$badblood = Read-Host -Prompt "Type `'badblood`' to deploy some randomness into a domain"
-$badblood.tolower()
-if($badblood -ne 'badblood'){exit}
-if($badblood -eq 'badblood'){
+{
    $Domain = Get-addomain
     Write-Progress -Activity "Random Stuff into A domain" -Status "Progress:" -PercentComplete ($i/$totalscripts*100)
 
